7d6dbac4b2cef91e5eecb1950410be927060c34a
AbigailKgotli <55486531+AbigailKgotli@users.noreply.github.com>
2019-10-04 18:39:03 +0200

    model added

--- JobIT/JobIT/Model1.Designer.cs
+++ JobIT/JobIT/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Lebo\Documents\SCHOOL\INFORMATICS\INF 272\INF272_Project\JobIT\JobIT\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ADMIN\Documents\INF272\INF272_Project\JobIT\JobIT\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

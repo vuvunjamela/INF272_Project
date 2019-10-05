34a80d200977fac561bdc59e0d76c764599fabc3
AbigailKgotli <55486531+AbigailKgotli@users.noreply.github.com>
2019-10-05 07:00:10 +0200

    new project with model

---
+++ NewProject_272/NewProject_272/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewProject_272.MvcApplication" Language="C#" %>
---
+++ NewProject_272/NewProject_272/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewProject_272.Startup))]
+namespace NewProject_272
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
